783ae2005a0d4133b5a63413463d22934ec7516c
Xiaofei <xifzhang@thoughtworks.com>
2016-10-19 14:15:35 +0800

    deploy@1019-v1

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+cp -r ../ptdev/build/* .
+echo "Please enter the version:"
+read version
+date_time=`date +'%m%d'`
+git add .
+git ci -am "deploy@${date_time}-${version}"
+git push origin master
+echo "Successfully publish!"
