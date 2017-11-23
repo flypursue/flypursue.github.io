a1b4dc0de57ad4352fd394e1770ec799248d4b72
Xiaofei <xifzhang@thoughtworks.com>
2017-11-23 17:50:01 +0800

    add main menu

--- src/containers/MainContent.js
+++ src/containers/MainContent.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-class App extends React.Component {
+class MainContent extends React.Component {
 
     render() {
         return (
             <div className="main">
+                <div className="menu">
+                    <ul>
+                        <li><a href="index.php">首页</a></li>
+                        <li><a href="/index.php?m=Product&amp;a=index&amp;id=3">产品展示</a></li>
+                        <li><a href="/index.php?m=Page&amp;a=index&amp;id=1">公司简介</a></li>
+                        <li><a href="/index.php?m=Page&amp;a=index&amp;id=6">联系我们</a></li>
+                    </ul>
+                </div>
             </div>
         );
     }
 }
 
-export default App;
+export default MainContent;
